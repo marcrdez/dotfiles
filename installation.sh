8269e041df4c185daa66c4b76f9673ed2bf5fc8b
Marc Rodriguez <rodriguezmoreno.marcos@gmail.com>
2024-05-03 23:20:45 +0200

    add script

---
+++ installation.sh
@@ -0,0 +1,19 @@
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+brew_install() {
+	echo "\nInstalling $1"
+	if brew list $1 &>/dev/null; then
+		echo "${1} is already installed"
+	else
+		brew install $1 && echo "$1 is installed"
+	fi
+}
+
+brew_install "nvim"
+brew_install "tmux"
+brew_install "git"
+brew_install "ripgrep"
+brew_install "stow"
+
+echo "\n${RED}Remember to install a Nerd Font (https://www.nerdfonts.com/font-downloads)${NC}"
