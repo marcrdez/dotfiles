74a166bdf1f3a8cdf38e7a0fab01754a44f29c62
Marc Rodríguez <marcosrodriguez@genially.com>
2024-05-03 11:24:22 +0200

    add nvim config

---
+++ nvim/.config/nvim/lua/plugins/tree.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+
+  config = function()
+    require("neo-tree").setup({
+      window = {
+        position = "right",
+      },
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>t", ":Neotree toggle<CR>", {
+      desc = "Toggle Neotree",
+      noremap = true,
+      silent = true,
+    })
+
+    vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+  end,
+}
