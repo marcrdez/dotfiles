6f94fe63b8cb154dfdc8d0a88b7724e121652487
Marc Rodriguez <rodriguezmoreno.marcos@gmail.com>
2024-09-28 16:58:45 +0200

    add fzf to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,3 +8,12 @@ alias ..="cd .."
 alias ...="cd ../.."
 
 alias l="ls -lF --color"
+
+# use neovim as default
+alias vi="nvim"
+alias vim="nvim"
+
+# update fzf bindings 
+fzf_configure_bindings --directory=\cf --git_status=\cg
+
+set -g FZF_PREVIEW_FILE_CMD "bat --style=numbers --color=always --line-range :500"
