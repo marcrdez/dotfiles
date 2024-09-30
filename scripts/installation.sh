f55c7a57562da26b3605f3acc50b3f5473904e99
Marc Rodríguez <marcosrodriguez@genially.com>
2024-09-30 09:53:32 +0200

    changes: include new files fish and move scripts

--- scripts/installation.sh
+++ scripts/installation.sh
@@ -2,12 +2,12 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 brew_install() {
-	echo "\nInstalling $1"
-	if brew list $1 &>/dev/null; then
-		echo "${1} is already installed"
-	else
-		brew install $1 && echo "$1 is installed"
-	fi
+  echo "\nInstalling $1"
+  if brew list $1 &>/dev/null; then
+    echo "${1} is already installed"
+  else
+    brew install $1 && echo "$1 installed"
+  fi
 }
 
 brew_install "nvim"
@@ -15,5 +15,7 @@ brew_install "tmux"
 brew_install "git"
 brew_install "ripgrep"
 brew_install "stow"
+brew_install "fish"
+brew_install "fisher"
 
 echo "\n${RED}Remember to install a Nerd Font (https://www.nerdfonts.com/font-downloads)${NC}"
