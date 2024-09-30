668b63f1a7aa5ee882d70b178a4afd31325f7bff
Marc Rodriguez <rodriguezmoreno.marcos@gmail.com>
2024-09-30 11:52:53 +0200

    export nvm and updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+export NVM_DIR=~/.nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 set fish_greeting "$(fish --version)"
 
 alias ..="cd .."
