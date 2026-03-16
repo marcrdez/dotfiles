6f882c87fca7840892fb2d4a4e31cd83c4d1a7f3
Marc Rodríguez <marc.rodriguez@embat.io>
2026-03-16 14:09:45 +0100

    changes fisher plugins and nvim linter

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,6 +6,8 @@ export NVM_DIR=~/.nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+set -gx PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
+
 set fish_greeting "$(fish --version)"
 
 alias ..="cd .."
@@ -29,6 +31,9 @@ fzf_configure_bindings --directory=\cf --git_status=\cg
 
 set -g FZF_PREVIEW_FILE_CMD "bat --style=numbers --color=always --line-range :500"
 
+# run fnm on init
+fnm env | source
+
 # pnpm
 set -gx PNPM_HOME /Users/marc/Library/pnpm
 if not string match -q -- $PNPM_HOME $PATH
@@ -39,3 +44,8 @@ end
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# start starship prompt if installed
+if command -q starship
+    starship init fish | source
+end
